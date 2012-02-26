2177056cbaee2a522a0374b9fd0198bff0df496b
Se7en <Seven@seven-home>
2012-02-26 21:53:20 +0100

    Unit test controller

--- controller/test.php
+++ controller/test.php
@@ -1,82 +1,90 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Test extends CI_Controller {
-
+	
 	function __construct()
-	{
-		parent::__construct();
+    {
+        parent::__construct(); 
 		$this->load->database();
+		$this->load->library('unit_test');
 		$this->load->model("test_model");
 	}
-
-	/* CREATE */
-	function insert(){
-		var_dump($this->test_model->insert(array(
+	
+	function index(){
+		/* CREATE */
+		$id=$this->test_model->insert(array(
 			"name"=>"Test",
 			"number"=>123456,
 			"date"=>date("Y-m-d")
-		)));
-	}
-	/* READ */
-	function find($id){
-		var_dump($this->test_model->find($id));
-	}
-	function find_by_name($name){
-		var_dump($this->test_model->find_by_name($name));
-	}
-	function find_where(){
-		var_dump($this->test_model->find_where(array(
-			"name"=>"Test",
-			"number"=>123456
-		)));
-	}
-	function find_all(){
-		var_dump($this->test_model->find_all());
-	}
-
-	/* UPDATE */
-	function update($id){
-		var_dump($this->test_model->update($id,array(
-			"name"=>"Test2"
-			)));
-	}
-	function update_by_name($name){
-		var_dump($this->test_model->update_by_name($name,array(
-			"name"=>"Test"
-			)));
-	}
-	function update_where(){
-		var_dump($this->test_model->update_where(array(
-			"name"=>"Test",
-			"number"=>123456
-		),array(
-			"name"=>"Test2"
-			)));
-	}
-
-	/* DELETE */
-	function delete($id){
-		var_dump($this->test_model->delete($id));
-	}
-	function delete_by_name($name){
-		var_dump($this->test_model->delete_by_name($name));
-	}
-	function delete_where(){
-		var_dump($this->test_model->delete_where(array(
-			"name"=>"Test",
-			"number"=>123456
-		)));
-	}
-
-	/* ORM */
-	function save(){
-		var_dump($this->test_model->save(array(
+		));
+		$this->unit->run($id, "is_numeric", "Insert record id=$id");
+		
+		$id2=$this->test_model->save(array(
 			"name"=>"Test2",
-			"number"=>1234567,
+			"number"=>654321,
 			"date"=>date("Y-m-d")
-		)));
-	}
+		));
+		$this->unit->run($id2, "is_numeric", "Save record id=$id2");
+		
+		/* READ */
+		$result=$this->test_model->find($id);
+		$this->unit->run(count($result), 1, "Find record with id=$id",json_encode($result));
+		
+		$result=$this->test_model->find(array($id,$id2));
+		$this->unit->run(count($result), 2, "Find record with id in $id,$id2",json_encode($result));
+		
+		$result=$this->test_model->find(9999);
+		$this->unit->run(count($result), 0, "Not find record with id=9999");
+				
+		$result=$this->test_model->find_by_name("Test");
+		$this->unit->run(count($result), 1, "Find record with name=Test",json_encode($result));
+		
+		$result=$this->test_model->find_by_name("Tset");
+		$this->unit->run(count($result), 0, "Not find record with name=Tset");
+		
+		$result=$this->test_model->find_where(array("date"=>date("Y-m-d")));
+		$this->unit->run(count($result), 2, "Find record with date=".date("Y-m-d"),json_encode($result));	
+
+		$result=$this->test_model->find_where(array("number"=>12));
+		$this->unit->run(count($result), 0, "Not find record with number=12");		
+		
+		$result=$this->test_model->find_all();
+		$this->unit->run(count($result), 2, "Find all record",json_encode($result));		
+		
+		/* UPDATE */
+		$result=$this->test_model->update($id,array(
+			"name"=>"TestTest"
+		));		
+		$this->unit->run(TRUE, TRUE, "Update record with id=$id",json_encode($this->test_model->find($id)));
+
+		$result=$this->test_model->update_by_name("TestTest",array(
+			"name"=>"Test"
+		));		
+		$this->unit->run(TRUE, TRUE, "Update record with name=TestTest",json_encode($this->test_model->find_by_name("Test")));	
+
+		
+		$result=$this->test_model->update_where(array("date"=>date("Y-m-d")),array(
+			"name"=>"TestAll"
+		));		
+		$this->unit->run(TRUE, TRUE, "Update record with date=".date("Y-m-d"),json_encode($this->test_model->find_by_name("TestAll")));	
+		
+		$result=$this->test_model->save(array(
+			"id"=>$id,
+			"name"=>"TestSave"
+		));		
+		$this->unit->run(TRUE, TRUE, "Save/Update record",json_encode($this->test_model->find($id)));					
+		
+		/* DELETE */
+		$result=$this->test_model->delete($id);
+		$this->unit->run($result, TRUE, "Delete record with id=$id",json_encode($this->test_model->find_all()));
+		
+		$result=$this->test_model->delete_by_name("TestAll");
+		$this->unit->run($result, TRUE, "Delete record with name=TestAll",json_encode($this->test_model->find_all()));			
+		
+		/*RESULT*/
+		echo $this->unit->report();
+	}			
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file test.php */
+/* Location: ./application/controllers/test.php */
